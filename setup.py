b0cf242a3faf978bbef5d34a27e4a161dc8b3c42
sjm <96165764+HeroxXd@users.noreply.github.com>
2022-02-21 09:36:21 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+import os
+print("[+] Installing requierments ...")
+os.system('pip install telethon==1.24.0')
+os.system('pip install colorama==0.4.3')
+os.system('pip install BeastX==0.3')
+
+print("[+] setup complete !")
+print("[+] now you can run any tool !")
+input('\n Press enter to exit...')
+#requirements
